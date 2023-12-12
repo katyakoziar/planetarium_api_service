27d82fae2a5df0ae5dc993ea052b0425763e7f5d
Kateryna Koziar <katya.kozyar.333@gmail.com>
2023-12-12 18:35:47 +0200

    Add urls for all endpoints to planetarium service

---
+++ planetarium/urls.py
@@ -0,0 +1,18 @@
+from rest_framework import routers
+from django.urls import path, include
+
+from planetarium.views import ShowThemeViewSet, PlanetariumDomeViewSet, AstronomyShowViewSet, ShowSessionViewSet, \
+    ReservationViewSet
+
+router = routers.DefaultRouter()
+router.register("show_themes", ShowThemeViewSet)
+router.register("planetarium_domes", PlanetariumDomeViewSet)
+router.register("astronomy_shows", AstronomyShowViewSet)
+router.register("show_sessions", ShowSessionViewSet)
+router.register("reservations", ReservationViewSet)
+
+urlpatterns = [
+    path("", include(router.urls))
+]
+
+app_name = "planetarium"
